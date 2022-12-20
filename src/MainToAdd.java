c169b08aa55df8064da755f5a11bb89ab3e7bece
MeesCode1 <meesie_1996@hotmail.com>
2022-12-20 02:30:11 +0100

    push nummer 2

---
+++ src/MainToAdd.java
@@ -0,0 +1,6 @@
+public class MainToAdd {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("deze code is gepusht naar github");
+    }
+}
